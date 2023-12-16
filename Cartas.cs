d05e4bb394fd861125d62d21bbc3e2d8656bf179
Pedro Dias <63060088+pedrexds@users.noreply.github.com>
2023-12-15 23:58:27 -0400

    Create Cartas.cs

---
+++ Cartas.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FireWall_Reigns
+{
+    public class Cartas
+    {
+        private string fato;
+        private int ptsPriv;
+        private int ptsRecs;
+        private int ptsSegu;
+        private int ptsUser;
+
+        public Cartas(string acontecimento, int p1, int p2, int p3, int p4)
+        {
+            this.fato = acontecimento;
+            this.ptsPriv = p1;
+            this.ptsRecs = p2;
+            this.ptsSegu = p3;
+            this.ptsUser = p4;
+        }
+
+        public void aplicaPontuacao(Entidades privacidade, Entidades recursos, Entidades seguranca, Entidades usuarios)
+        {
+            privacidade.setPontuacao(ptsPriv);
+            recursos.setPontuacao(ptsRecs);
+            seguranca.setPontuacao(ptsSegu);
+            usuarios.setPontuacao(ptsUser);
+        }
+    }
+}
