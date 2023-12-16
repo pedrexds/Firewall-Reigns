c3632969e43541f53f55f2aa1290796b7c49bfc1
Pedro Dias <63060088+pedrexds@users.noreply.github.com>
2023-12-15 23:59:42 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,6 +6,9 @@ namespace FireWall_Reigns
     {
         static void Main(string[] args)
         {
+            int resposta1;
+            Cartas instagramRealOp1 = new Cartas("Perfil Público", -5, -5, -10, 0);
+            Cartas instagramRealOp2 = new Cartas("Perfil Privado", 5, -5, 0, -20);
             GameController gc = new GameController(1);
             Entidades privacidade = new Entidades(50);
             Entidades recursos = new Entidades(50);
@@ -13,12 +16,17 @@ namespace FireWall_Reigns
             Entidades usuarios = new Entidades(50);
             
             gc.mostraPontuacao(privacidade, recursos, seguranca, usuarios);
-            Console.WriteLine();
-
-            privacidade.setPontuacao(-5);
-            recursos.setPontuacao(50);
-            seguranca.setPontuacao(-25);
-            usuarios.setPontuacao(12);
+            
+            Console.WriteLine("Instagram Real: Opção 1 ou 2?");
+            resposta1 = Int32.Parse(Console.ReadLine());
+            if(resposta1 == 1)
+            {
+                instagramRealOp1.aplicaPontuacao(privacidade, recursos, seguranca, usuarios);
+            }
+            else
+            {
+                instagramRealOp2.aplicaPontuacao(privacidade, recursos, seguranca, usuarios);
+            }
 
             gc.conferePontuacao(privacidade, recursos, seguranca, usuarios);
 
