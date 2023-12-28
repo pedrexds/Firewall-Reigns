1d7903893a462520ca69d8c6d5ed883fe24b1199
Pedro Dias <63060088+pedrexds@users.noreply.github.com>
2023-12-28 19:09:10 -0400

    Update Cartas.cs

--- Cartas.cs
+++ Cartas.cs
@@ -7,27 +7,60 @@ namespace FireWall_Reigns
 {
     public class Cartas
     {
-        private string fato;
-        public int ptsPriv;
-        public int ptsRecs;
-        public int ptsSegu;
-        public int ptsUser;
+        private int numeroDaCarta;
+        public string fato;
+        public string opcao1;
+        public string opcao2;
+        public int ptsPrivOp1;
+        public int ptsRecsOp1;
+        public int ptsSeguOp1;
+        public int ptsUserOp1;
+        public int ptsPrivOp2;
+        public int ptsRecsOp2;
+        public int ptsSeguOp2;
+        public int ptsUserOp2;
+        public bool escolhido1;
+        public bool escolhido2;
 
-        public Cartas(string acontecimento, int p1, int p2, int p3, int p4)
+        public Cartas(int numCarta, string acontecimento, string opt1, int priv1, int recs1, int segu1, int user1, string opt2, int priv2, int recs2, int segu2, int user2, bool choice1, bool choice2)
         {
+            this.numeroDaCarta = numCarta;
             this.fato = acontecimento;
-            this.ptsPriv = p1;
-            this.ptsRecs = p2;
-            this.ptsSegu = p3;
-            this.ptsUser = p4;
+            this.opcao1 = opt1;
+            this.ptsPrivOp1 = priv1;
+            this.ptsRecsOp1 = recs1;
+            this.ptsSeguOp1 = segu1;
+            this.ptsUserOp1 = user1;
+            this.opcao2 = opt2;
+            this.ptsPrivOp2 = priv2;
+            this.ptsRecsOp2 = recs2;
+            this.ptsSeguOp2 = segu2;
+            this.ptsUserOp2 = user2;
+            this.escolhido1 = choice1;
+            this.escolhido2 = choice2;
         }
 
-        public void aplicaPontuacao(Entidades privacidade, Entidades recursos, Entidades seguranca, Entidades usuarios)
+        public void aplicaPontuacao(Entidades privacidade, Entidades recursos, Entidades seguranca, Entidades usuarios, bool option1, bool option2)
         {
-            privacidade.setPontuacao(ptsPriv);
-            recursos.setPontuacao(ptsRecs);
-            seguranca.setPontuacao(ptsSegu);
-            usuarios.setPontuacao(ptsUser);
+            if(option1)
+            {
+                privacidade.setPontuacao(this.ptsPrivOp1);
+                recursos.setPontuacao(this.ptsRecsOp1);
+                seguranca.setPontuacao(this.ptsSeguOp1);
+                usuarios.setPontuacao(this.ptsUserOp1);
+            }
+            else if(option2)
+            {
+                privacidade.setPontuacao(this.ptsPrivOp2);
+                recursos.setPontuacao(this.ptsRecsOp2);
+                seguranca.setPontuacao(this.ptsSeguOp2);
+                usuarios.setPontuacao(this.ptsUserOp2);        
+            }
+        }
+
+        public void imprimirInfoCarta()
+        {
+            Console.WriteLine(this.fato + "\n\n1 - " + this.opcao1 + "\n\n2 - " + this.opcao2 + "\n\n");
         }
     }
 }
